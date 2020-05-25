c75a1b0bb32921d70d00ef5899a73246c7c56aa3
Artsiom Bakhurynski <55259593+ArtsiomBax@users.noreply.github.com>
2020-05-25 12:02:42 +0300

    Add files via upload

---
+++ src/main/java/by/artbax/model/WeatherModel.java
@@ -0,0 +1,50 @@
+package by.artbax.model;
+
+public class WeatherModel {
+
+    private String name;
+    private String icon;
+    private String main;
+    private Double temp;
+    private Double humidity;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getIcon() {
+        return icon;
+    }
+
+    public void setIcon(String icon) {
+        this.icon = icon;
+    }
+
+    public String getMain() {
+        return main;
+    }
+
+    public void setMain(String main) {
+        this.main = main;
+    }
+
+    public Double getTemp() {
+        return temp;
+    }
+
+    public void setTemp(Double temp) {
+        this.temp = temp;
+    }
+
+    public Double getHumidity() {
+        return humidity;
+    }
+
+    public void setHumidity(Double humidity) {
+        this.humidity = humidity;
+    }
+}
